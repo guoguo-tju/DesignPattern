424161eb10222ea096ba4485bd40a018a82860a0
guozhaorong <guozhaorong@zichan360.com>
2018-12-14 14:39:16 +0800

    模板方法模式

---
+++ src/main/java/com/guoguo/designpatter/template/TemplateTest.java
@@ -0,0 +1,19 @@
+package com.guoguo.designpatter.template;
+
+/**
+ * @program: DesignPattern
+ * @description:
+ * @author: Karl Guo
+ * @create: 2018-12-14 14:02
+ **/
+public class TemplateTest {
+
+    public static void main(String[] args) {
+
+        DemoTemplate demoTemplate = new DemoTemplateImpl();
+
+        demoTemplate.demoStepTemplate();
+
+    }
+
+}
